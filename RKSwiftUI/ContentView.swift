ccdd868ec3caca63fdc4eee0e258a7f1c4da5111
Peter Rogers <p.rogers@gold.ac.uk>
2023-02-20 13:39:32 +0000

    getting swiftUI intro

--- RKSwiftUI/ContentView.swift
+++ RKSwiftUI/ContentView.swift
@@ -27,14 +27,8 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        boxAnchor.actions.hitCube.onAction = {entity in
-                   model?.text = "model hit"
-               }
-        
+        model?.arView = arView
+        model?.initScene()
         return arView
     }
     
--- RKSwiftUI/MyModel.swift
+++ RKSwiftUI/MyModel.swift
@@ -7,11 +7,27 @@
 
 import Foundation
 import SwiftUI
+import RealityKit
 
 class MyModel: ObservableObject{
     @Published var text = "Nothing Hit"
+    weak var arView:ARView?
     
     init(){
         
     }
+    
+    func initScene(){
+        let boxAnchor = try! Experience.loadBox()
+        // Add the box anchor to the scene
+        print("foofo")
+        arView?.scene.anchors.append(boxAnchor)
+        boxAnchor.actions.hitCube.onAction = {entity in
+            self.text = "model hit\(Int.random(in: 0 ... 10))"
+               }
+    }
+    
+  
+    
+    
 }
